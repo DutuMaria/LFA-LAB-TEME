7703c135601e6f16091e2322989a4a1d86dfc4fa
Maria Dutu <maria.dutu@s.unibuc.ro>
2021-03-30 17:13:38 +0300

    mici modificari cod

--- Proiect1/main.py
+++ Proiect1/main.py
@@ -5,13 +5,14 @@ def verificare(cuvant):
     traseu.append(stare_curenta)
     rezultat_coduri.append(coduri[int(stare_curenta)])
     index = 0
+
     while index < len(cuvant):
         ok = 0
         transition = 0
-        while transition < len(Tranzitii):
-            if Tranzitii[transition][0] == stare_curenta and cuvant[index] == Tranzitii[transition][2]:
-                stare_curenta = Tranzitii[transition][1]
-                transition = len(Tranzitii)
+        while transition < len(tranzitii):
+            if tranzitii[transition][0] == stare_curenta and cuvant[index] == tranzitii[transition][2]:
+                stare_curenta = tranzitii[transition][1]
+                transition = len(tranzitii)
                 rezultat_coduri.append(coduri[int(stare_curenta)])
                 traseu.append(stare_curenta)
                 ok = 1
@@ -21,43 +22,43 @@ def verificare(cuvant):
             return 0
 
         index += 1
-
+        
     if stare_curenta in stari_fin:
-        return (1, rezultat_coduri, traseu)
+        return 1, rezultat_coduri, traseu
     return 0
 
 
-with open ("input.txt") as f:
-    line = f.readline().strip()
-    nr_noduri, nr_tranzitii = line.split()
-    nr_noduri = int(nr_noduri)
-    nr_tranzitii = int(nr_tranzitii)
-
-    line = f.readline().strip()
-    coduri = [cod for cod in line.split()]
-
-    Tranzitii = []
-
-    for i in range(nr_tranzitii):
+if __name__ == "__main__":
+    with open("input.txt") as f:
         line = f.readline().strip()
-        stare_init, stare_fin, lit = line.split()
-        Tranzitii.append((stare_init, stare_fin, lit))
+        nr_noduri, nr_tranzitii = line.split()
+        nr_noduri = int(nr_noduri)
+        nr_tranzitii = int(nr_tranzitii)
+        tranzitii = []
 
-    stare_init = f.readline().strip()
-    stari_fin = f.readline().strip().split()
-    nr_stari_fin = stari_fin[0]
-    stari_fin = stari_fin[1:]
-    nr_cuv = int(f.readline().strip())
-    cuvinte = []
+        line = f.readline().strip()
+        coduri = [cod for cod in line.split()]
 
-    for i in range(nr_cuv):
-        cuvinte.append(f.readline().strip())
+        for i in range(nr_tranzitii):
+            line = f.readline().strip()
+            stare_init, stare_fin, lit = line.split()
+            tranzitii.append((stare_init, stare_fin, lit))
 
-    for cuv in cuvinte:
-        if verificare(cuv):
-            rezultat = verificare(cuv)
-            print("DA")
-            print(" ".join(rezultat[1]))
-            print("Traseu:", " ".join(rezultat[2]))
-        else:
-            print("NU")
+        stare_init = f.readline().strip()
+        stari_fin = f.readline().strip().split()
+        nr_stari_fin = stari_fin[0]
+        stari_fin = stari_fin[1:]
+        nr_cuv = int(f.readline().strip())
+        cuvinte = []
+
+        for i in range(nr_cuv):
+            cuvinte.append(f.readline().strip())
+
+        for cuv in cuvinte:
+            if verificare(cuv):
+                rezultat = verificare(cuv)
+                print("DA")
+                print(" ".join(rezultat[1]))
+                print("Traseu:", " ".join(rezultat[2]))
+            else:
+                print("NU")
